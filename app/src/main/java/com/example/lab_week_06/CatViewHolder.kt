e7904fccc6a52f8d8e5bc6db3d7fd2bef033cf65
tukikaru <patrickalviant@gmail.com>
2025-10-05 00:42:44 +0700

    update catviewholder catadapter

---
+++ app/src/main/java/com/example/lab_week_06/CatViewHolder.kt
@@ -0,0 +1,41 @@
+package com.example.lab_week_06
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lab_week_06.model.CatBreed
+import com.example.lab_week_06.model.CatModel
+import com.example.lab_week_06.model.Gender
+
+private val FEMALE_SYMBOL = "\u2640"
+private val MALE_SYMBOL = "\u2642"
+private const val UNKNOWN_SYMBOL = "?"
+
+class CatViewHolder(
+    containerView: View,
+    private val imageLoader: ImageLoader
+) : RecyclerView.ViewHolder(containerView) {
+
+    private val catPhotoView: ImageView by lazy { containerView.findViewById(R.id.cat_photo) }
+    private val catNameView: TextView by lazy { containerView.findViewById(R.id.cat_name) }
+    private val catBreedView: TextView by lazy { containerView.findViewById(R.id.cat_breed) }
+    private val catBiographyView: TextView by lazy { containerView.findViewById(R.id.cat_biography) }
+    private val catGenderView: TextView by lazy { containerView.findViewById(R.id.cat_gender) }
+
+    fun bindData(cat: CatModel) {
+        imageLoader.loadImage(cat.imageUrl, catPhotoView)
+        catNameView.text = cat.name
+        catBreedView.text = when (cat.breed) {
+            CatBreed.AmericanCurl -> "American Curl"
+            CatBreed.BalineseJavanese -> "Balinese-Javanese"
+            CatBreed.ExoticShorthair -> "Exotic Shorthair"
+        }
+        catBiographyView.text = cat.biography
+        catGenderView.text = when (cat.gender) {
+            Gender.Female -> FEMALE_SYMBOL
+            Gender.Male -> MALE_SYMBOL
+            else -> UNKNOWN_SYMBOL
+        }
+    }
+}
